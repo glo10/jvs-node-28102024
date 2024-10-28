056459a6dfcecdb10ab1550a35f0bb38a6c38d96
glodie <glodie.tshimini@gmail.com>
2024-10-28 15:18:19 +0100

    refactor: update exercise 3

--- 0-exercices/corrections/ex1/index.mjs
+++ 0-exercices/corrections/ex1/index.mjs
@@ -2,6 +2,14 @@ import { stdin as input, stdout as output } from 'process'
 import { createInterface } from 'readline/promises'
 
 const app = createInterface({ input, output })
+/**
+ * Autre possibilité
+ * pour la ligne 4
+ * 1. Supprimer la ligne 1 d'import depuis 'process'
+ * 2. Remplacer la ligne 4 par le code ci-dessous dans les commentaires
+ * 
+ * const app = createInterface({ input: process.stdin, output: process.stdout })
+ */
 const lastName = await app.question('Quel est votre nom ? ')
 const firstName = await app.question('Quel est votre prénom ? ')
 console.log(`Bonjour ${lastName} ${firstName} !`)
