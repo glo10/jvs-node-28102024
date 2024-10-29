95f7afb471178f57ac8cb3579ec3a55a8e5624bd
glodie <glodie.tshimini@gmail.com>
2024-10-29 15:31:34 +0100

    refactor: update correction ex5

--- 0-exercices/corrections/ex5/src/app.mjs
+++ 0-exercices/corrections/ex5/src/app.mjs
@@ -7,7 +7,7 @@ const app = createServer((req, res) => {
   const currentFilePath = fileURLToPath(import.meta.url) // en ECMASCRIPT import.meta.url contient le chemin absolue du fichier en cours
   // En commonJS la variable globale __dirname permet de récupérer le chemin absolu du dossier du programme en cours
   const currentFileDir = dirname(currentFilePath)
-  const page404 = resolve(currentFileDir, 'views', '404.html')
+  const PAGE_404 = resolve(currentFileDir, 'views', '404.html')
   const imgRgx = /.+\.(jpg|jpeg|png)$/
   const { url } = req
   let type = {'Content-Type' : 'text/html'}
@@ -19,16 +19,24 @@ const app = createServer((req, res) => {
     filename = join(currentFileDir, 'views', url)
     type = {'Content-Type' : 'text/css'}
   }
+  // idem que if(url.endWith('jpg) || url.endWith('png) || (url.endWith('jpeg) )
   else if(imgRgx.test(url)) {
-    const matches = url.match(/.+\.(jpg|jpeg|png)$/)
+    // La méthode match() retourne un tableau avec tous les morceaux qui match avec l'expression régulière
+    const matches = url.match(imgRgx)
+    // Récupération de l'extension à l'index 1, index 0 on a l'url de l'image
     const ext = matches[1]
     filename = join(currentFileDir, 'views', url)
     type = {'Content-Type' : `image/${ext}`}
   }
+  /**
+   * Le navigateur lorsqu'on rend du HTML
+   *  effectue une requête supplémentaire pour charger le favicon.ico 
+   *  qui se trouve à la racine du site Web
+   */
   else if(/favicon\.ico/.test(url)) {
     return
   }
-  render(filename, page404, res, type)
+  render(filename, PAGE_404, res, type)
 })
 
 export default app
