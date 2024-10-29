3fda208024f3911ab9ffe1b8fc3fe12046b6145b
glodie <glodie.tshimini@gmail.com>
2024-10-29 11:03:18 +0100

    feat: demo web server again

---
+++ demo/8-rappels-web-server/server.js
@@ -0,0 +1,25 @@
+import { createServer } from 'node:http'
+const PORT = 8060
+createServer((req, res) => {
+    // callback fonction qui s'exécute lorsqu'un client se connecte
+    /**
+     * Les infos liées à la requête tels que url : req.url
+     *  les paramètres (paramètres d'URL ou paramètres avec GET) : req.query ou req.params
+     * Le body (infos envoyés en POST) : req.body
+     * IP : req.ip
+     * method : req.method
+     * nom de domaine : req.host
+     */
+    /**
+     * Un objet de type Response pour retourner une réponse au client
+     */
+    if(req.url === '/') {
+        res.writeHead(200, { 'Content-Type' : 'text/html'})
+        res.end('<h1>Page d\'acceuil</h1>')
+    } else {
+        res.writeHead(404, { 'Content-Type' : 'text/html'})
+        res.end('<h1>Page 404</h1>')
+    }
+}).listen(PORT, () => {
+    console.log(`Running on http://localhost:${PORT}`)
+})
